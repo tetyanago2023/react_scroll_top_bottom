770c026f2f7759df366a6d9f063d82da857537d9
go2023 <tetyanago2018@gmail.com>
2024-02-25 16:07:45 -0800

    functionality was implemented

--- src/components/scroll-to-top-and-bottom/index.jsx
+++ src/components/scroll-to-top-and-bottom/index.jsx
@@ -1,16 +1,29 @@
 import useFetch from "../use-fetch";
+import {useRef} from "react";
 
 const ScrollToTopAndBottom = () => {
     const { data, error, loading } = useFetch(
-        "https://dummyjson.com/products",
+        "https://dummyjson.com/products?limit=100",
         {}
     );
+    const bottomRef = useRef(null);
+    const handleScrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+        });
+    };
+
+    const handleScrollToBottom = () => {
+        bottomRef.current.scrollIntoView({ behavior: "smooth" });
+    };
 
     return (
         <div>
             <h1>Scroll To Top And Bottom Feature</h1>
             <h3>This is the top of the section</h3>
-            <button>Scroll to bottom</button>
+            <button onClick={handleScrollToBottom}>Scroll to bottom</button>
             {loading ? <h3>Loading...</h3> : null}
             {error ? <h3>{error}</h3> : null}
             {data && data.products && data.products.length
@@ -20,7 +33,8 @@ const ScrollToTopAndBottom = () => {
                     </p>
                 ))
                 : null}
-            <button>Scroll to top</button>
+            <button onClick={handleScrollToTop}>Scroll to top</button>
+            <div ref={bottomRef}></div>
             <h3>This is the bottom the section</h3>
         </div>
     );
